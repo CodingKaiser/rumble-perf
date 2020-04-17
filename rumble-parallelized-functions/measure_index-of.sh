b507a9bb0fd086e76239dd60b25d2c600046a0e1
Falko Noe <falkonoe@gmail.com>
2020-04-17 16:59:57 +0200

    Added index-of file

---
+++ rumble-parallelized-functions/measure_index-of.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -f ./log-io-*.txt
+
+reps=5
+
+> ./log-io-p.txt
+
+for i in `seq 1 $reps`;
+do
+  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.5.1.jar --query-path iniox-of1.jq --output-path output-io-p --log-path ./log-io-p-tmp.txt --overwrite yes --result-size 100000000
+  cat ./log-io-p-tmp.txt >> ./log-io-p.txt
+done
+
+> ./log-io-l.txt
+
+for i in `seq 1 $reps`;
+do
+  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.4.2.jar --query-path iniox-of1.jq --output-path output-io-l --log-path ./log-io-l-tmp.txt --overwrite yes --result-size 100000000
+  cat ./log-io-l-tmp.txt >> ./log-io-l.txt
+done
