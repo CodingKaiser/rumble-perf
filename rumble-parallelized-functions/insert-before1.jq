5cb2e45c5431e9f39f580a6bc8fc54c8e59c9f07
Falko Noe <falkonoe@gmail.com>
2020-04-17 16:01:54 +0200

    Decreased sizes of inputs to allow for materialization

--- rumble-parallelized-functions/insert-before1.jq
+++ rumble-parallelized-functions/insert-before1.jq
@@ -1,5 +1,5 @@
-insert-before(
-	json-file("../../sample_dataset/confusion-2014-03-02/confusion-2014-03-02.json"), 
+empty(insert-before(
+	json-file("../../sample_dataset/confusion-2014-03-02/confusion-2014-03-02-tenth.json"), 
 	100000,
 	json-file("../../sample_dataset/confusion-2014-03-02/confusion-2014-03-02-tenth.json")
-)
+))
--- rumble-parallelized-functions/measure_insert-before1.sh
+++ rumble-parallelized-functions/measure_insert-before1.sh
@@ -8,7 +8,7 @@ reps=5
 
 for i in `seq 1 $reps`;
 do
-  spark-submit --driver-memory 9g --master local[*] spark-rumble-1.5.1.jar --query-path insert-before1.jq --output-path output-ib-p --log-path ./log-ib-p-tmp.txt --overwrite yes
+  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.5.1.jar --query-path insert-before1.jq --output-path output-ib-p --log-path ./log-ib-p-tmp.txt --overwrite yes --result-size 100000000
   cat ./log-ib-p-tmp.txt >> ./log-ib-p.txt
 done
 
@@ -16,6 +16,6 @@ done
 
 for i in `seq 1 $reps`;
 do
-  spark-submit --driver-memory 9g --master local[*] spark-rumble-1.4.2.jar --query-path insert-before1.jq --output-path output-ib-l --log-path ./log-ib-l-tmp.txt --overwrite yes --result-size 100000000
+  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.4.2.jar --query-path insert-before1.jq --output-path output-ib-l --log-path ./log-ib-l-tmp.txt --overwrite yes --result-size 100000000
   cat ./log-ib-l-tmp.txt >> ./log-ib-l.txt
 done
