c6f5f9c5339eaf4417ce15544c53a7cc3b39724e
Falko Noe <falkonoe@gmail.com>
2020-04-20 18:10:01 +0200

    Added a second test

--- rumble-parallelized-functions/measure_index-of1.sh
+++ rumble-parallelized-functions/measure_index-of1.sh
@@ -2,22 +2,22 @@
 
 rm -f ./log-io-*.txt
 
-reps=25
+reps=3
 
-> ./log-io-p.txt
+> ./log-io-p-1.txt
 
 for i in `seq 1 $reps`;
 do
-  rm -rf ./output-io-p
-  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.5.1.jar --query-path index-of1.jq --output-path output-io-p --log-path ./log-io-p-tmp.txt --overwrite yes --result-size 100000000
-  cat ./log-io-p-tmp.txt >> ./log-io-p.txt
+  rm -rf ./output-io-p-1
+  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.5.1.jar --query-path index-of1.jq --output-path output-io-p-1 --log-path ./log-io-p-1-tmp.txt --overwrite yes --result-size 100000000
+  cat ./log-io-p-1-tmp.txt >> ./log-io-p-1.txt
 done
 
-> ./log-io-l.txt
+> ./log-io-l-1.txt
 
 for i in `seq 1 $reps`;
 do
-  rm -rf ./output-io-l
-  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.4.2.jar --query-path index-of1.jq --output-path output-io-l --log-path ./log-io-l-tmp.txt --overwrite yes --result-size 100000000
-  cat ./log-io-l-tmp.txt >> ./log-io-l.txt
+  rm -rf ./output-io-l-1
+  spark-submit --driver-memory 16g --master local[*] spark-rumble-1.4.2.jar --query-path index-of1.jq --output-path output-io-l-1 --log-path ./log-io-l-1-tmp.txt --overwrite yes --result-size 100000000
+  cat ./log-io-l-1-tmp.txt >> ./log-io-l-1.txt
 done
